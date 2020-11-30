622dc4bb18b23cdb41f67c8360182333f17aa9aa
fuzzy0906 <41178191+fuzzy0906@users.noreply.github.com>
2020-11-30 08:28:06 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+a = 'hello'
+b = "Python"
+
+d = '%s %s %f' % (a, b, 3.7)    
+print(d)            # hello Python 3.700000
---
+++ main.ts
@@ -0,0 +1,4 @@
+let a = "hello"
+let b = "Python"
+let d = `${a} ${b} ${3.7}`
+console.log(d)
